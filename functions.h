0736f5405dfff29851c185b7da9e037486cbf5ac
Alexis Fernandez <99295832+alexis-fernandez@users.noreply.github.com>
2024-01-25 21:51:08 -0500

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,8 +1,7 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-// ###########################################################
-// Do not change this part
+
 typedef struct {
     double *csr_data;   // Array of non-zero values
     int *col_ind;       // Array of column indices
@@ -27,15 +26,6 @@ void solverJacobi(const CSRMatrix *A, double *b, double *x, int max_iterations, 
 void compute_residual(const CSRMatrix *A, const double *x, double *y, const double *b, double *r);
 double compute_norm(const CSRMatrix *A, const double *r);
 
-/* <Here you can add the declaration of functions you need.>
-<The actual implementation must be in functions.c>
-Here what "potentially" you need:
-1. A function called "solver" receiving const CSRMatrix A, double *b, double *x 
-and solving the linear system 
-2. A function called "compute_residual" to compute the residual like r=Ax-b.
-This shows how much x values you found are accurate, but 
-printing the whole vector r might not be a good idea. So
-3. A function called compute_norm to compute the norm of vector residual
-*/
+
 
 #endif
